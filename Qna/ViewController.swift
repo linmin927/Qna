5572000a4d89c775855334eb7b36cd9c2b93aacf
linmin <shawnlin0927@gmail.com>
2019-08-27 20:41:26 +0800

    Initial Commit

---
+++ Qna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qna
+//
+//  Created by 林銘 on 2019/8/27.
+//  Copyright © 2019 林家銘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
